43ecb57d3267768a97a2e5b35185ff4053b7f50a
Pall Pandiyan.S <pallpandiyan@gmail.com>
2024-04-21 13:48:39 +0530

    implement print_time()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use chrono::{Timelike, Utc};
+
+fn print_time() {
+    let now = Utc::now();
+    let (is_pm, hour) = now.hour12();
+    println!(
+        "Time now: {:2}:{:2}:{:2} {}",
+        hour,
+        now.minute(),
+        now.second(),
+        if is_pm { "PM" } else { "AM" }
+    )
+}
+
 fn main() {
-    println!("Hello, world!");
+    print_time();
 }
